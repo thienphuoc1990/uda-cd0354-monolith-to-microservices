cef021d9984fa5e5fe80675dc0100f21a7a34cf2
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-09-17 22:16:54 +0700

    monoStarting: some changes for setup and run up project

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -7,7 +7,13 @@ export const sequelize = new Sequelize({
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
+  'port': 5432,
   'dialect': config.dialect,
   'storage': ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 });
