2fffc13e20dde14cce095cf03d8737be1755fc3e
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-09-18 10:53:43 +0700

    k8sDeploy: update for env prod

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: `http://${process.env.REVERSE_PROXY_IP}:8080/api/v0`
 };
 
 /*
