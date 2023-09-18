6c2f0f183fc403deed7343db8723413d16776de7
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-09-18 18:10:37 +0700

    debugS3Access: debug S3 access

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -4,6 +4,7 @@ import {config} from './config/config';
 
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
+console.log({credentials: credentials});
 AWS.config.credentials = credentials;
 
 export const s3 = new AWS.S3({
